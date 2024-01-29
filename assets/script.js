e2fedefebb81edeafde56fd07c03a14ce8202f41
Mark Barrie <mark.a.barrie@gmail.com>
2024-01-28 21:57:51 -0500

    finished logic for colors changing

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,17 @@
 var currentDayHeader = $('#currentDay')
 currentDayHeader.addClass('display-4')
 var currentDate = dayjs().format('MMMM D, YYYY');
+var currentTime = dayjs().format('HH:mm:ss');
+var hour = dayjs().hour();
+// var hour = 14
+
 
 $(function () {
-  
+
   setInterval(function () {
-    var currentTime = dayjs().format('hh:mm:ss');
+    currentTime = dayjs().format('HH:mm:ss');
     currentDate = dayjs().format('MMMM D, YYYY');
-    console.log(currentTime);
+    // console.log(currentTime);
     currentDayHeader.text(currentDate)
   }, 1000);
 
@@ -26,7 +30,7 @@ $(function () {
   $('.saveBtn').on('click', function () {
     var textareaValue = $(this).closest('.time-block').find('textarea').val();
     var divId = $(this).closest('.time-block').attr('id');
-    var currentDate = dayjs().format('MMMM D, YYYY');
+    currentDate = dayjs().format('MMMM D, YYYY');
 
     // Check if we already have an entry for the currentDate and parse it, or initialize an empty array
     // var currentEntries = JSON.parse(localStorage.getItem(currentDate));
@@ -56,25 +60,28 @@ $(function () {
     console.log('Updated localStorage:', JSON.parse(localStorage.getItem(currentDate)));
   });
 
+  var timeDivs = [];
+  // looping through divs via ID and placing them in an array
+  for (var i = 9; i <= 17; i++) {
+    var div = $('#' + i);
+    timeDivs.push(div);
+  }
+  // logic to handle adding and removing classes depending on current hour
+  for (var i = 0; i < timeDivs.length; i++) {
+    if (timeDivs[i].attr('id') < hour) {
+      timeDivs[i].addClass('past');
+      timeDivs[i].removeClass('present future');
+    } else if (timeDivs[i].attr('id') == hour) {
+      timeDivs[i].addClass('present');
+      timeDivs[i].removeClass('past future');
+    } else {
+      timeDivs[i].addClass('future');
+      timeDivs[i].removeClass('present past');
+    }
+  }
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+
 });
 
 
